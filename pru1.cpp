67a4967864e6cfb4a0886f739c1544bb5fd280ee
rulitoxd <rulitosxd1@gmail.com>
2019-02-20 10:44:45 -0500

    V0.2.35

--- pru1.cpp
+++ pru1.cpp
@@ -98,45 +98,6 @@ bool Edge::isConnected(){
 
 //------------------------Aca terminan las estructuras del grafo--------------------------
 //---------------------------------------------------------------------------------------
-//					Clase del QuadTree
-class qtree{
-	private:
-		vector<Point> points;
-		Rect frame;
-		unsigned capacity;
-		bool divided = false;
-
-	public://--------------------Acá definimos la forma de nuestro quadtree--------------------------------------------
-		qtree(Rect Frame, unsigned n){
-			this->frame = Frame;
-			this->capacity = n;
-		}
-
-//--------------------------------Ahora vamos a definir las funciones de qtree---------------------------------
-
-		void successor();
-		void predeccessor();
-		void divide(){
-			
-
-		}
-
-		void insert(Point x){
-			if(points.size() < capacity){
-				this->points.push_back(x);
-			}else {
-				if(!this->divided){
-				this->divide();
-				this->divided = true;
-				}
-			}
-		}
-
-
-		
-};
-//---------------------------Acá se acaba la clase qtree-------------------------------------------------
-//---------------------------------------------------------------------------------------
 int main(void){
 
 
