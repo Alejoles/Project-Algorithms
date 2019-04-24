10266c4ae6fb23c3c9fdf78a87148836d4be5a65
rulitoxd <rulitosxd1@gmail.com>
2019-04-23 22:44:59 -0500

    Working hard mdf

--- quadtree.cpp
+++ quadtree.cpp
@@ -1,4 +1,5 @@
 #include "quadtree.hh"
+#include <stdlib>
 
 //--------------------------METODOS DE LA CLASE POINT------------------------------
 
@@ -16,6 +17,11 @@ void Point::setPY(int v){
 	this->y = v;
 }
 
+bool Point::inBounds(Point p){
+	//Esta funcion nos dice si el punto pertenece a nuestra "base"
+	return (p.x >= ArribaIz.x && p.x <= AbajoDer.x && p.y >= ArribaIz.y && p.y <= AbajoDer.y);
+}
+
 
 //--------------------------------END OF POINT-----------------------------------
 
@@ -25,27 +31,58 @@ void Point::setPY(int v){
 
 
 // Metodos del qtree
-void qtree::insert(Point x){
-	if(points.size() < capacity){
-		this->points.push_back(x);
-	}else {
-		if(!this->divided){
-		this->divide();
-		this->divided = true;
-		}
+void Invariante() const{
+	if(root == nullptr){
+		std::cout << "Root == null pointer." << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	if(!inBounds(x)){
+		std::cout << "The point is out of bounds." << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	if(abs(topLeft.x - botRight.x) <= 1 && abs(topLeft.y - botRight.y) <= 1){
+		if(Qtree == nullptr){
+			Qtree = root;
+			std::cout << "The space given for the tree now is so small to continue. (you can expand the bounds)" << std::endl;
+			exit(EXIT_FAILURE);
 	}
 }
 
-bool qtree::inBounds(Point p){
-	//Esta funcion nos dice si el punto pertenece a nuestra "base"
-	return (p.x >= ArribaIz.x && p.x <= AbajoDer.x && p.y >= ArribaIz.y && p.y <= AbajoDer.y);
-}
-
-QtreeNode * Qtree::Search(){
+void Qtree::insert(QtreeNode * &root, Point x){
+	
 
 }
+void insert(Point p){
+
+}
+
+QtreeNode Qtree::find()const{
+	
+}
+
+void Qtree::clear(QtreeNode * &root){
+	if(Qtree != nullptr){
+		QtreeNode *NorthWest = Qtree->NorthWest;
+		QtreeNode *NorthEast = Qtree->NorthEast;
+		QtreeNode *SouthWest = Qtree->SouthWest;
+		QtreeNode *SouthEast = Qtree->SouthEast;
+		delete Qtree;
+		clear(NorthWest);
+		clear(NorthEast);
+		clear(SouthWest);
+		clear(SouthEast);
+	}
+}
+void Qtree::clear(void){
+	clear(Qtree);
+	Qtree = nullptr;
+	count = 0;
+}
 
 
 
+Qtree::~Qtree(){
+	clear();
+}
 
 
--- quadtree.hh
+++ quadtree.hh
@@ -9,6 +9,11 @@ using namespace std;
 struct Point {
 	private:
 		int x,y;
+		
+		//-------------- Bounds // Esquinas -------------
+		Point ArribaIz = Point(0,0);
+		Point Centro = Point(200,200);
+		Point AbajoDer = Point(400,400);
 	public:
 		Point() : x{0}, y{0} {}
 		Point(int nx, int ny) : x{nx}, y{ny} {}
@@ -17,6 +22,7 @@ struct Point {
 		int getPY();
 		void setPX(int v);
 		void setPY(int v);
+		bool inBounds(Point p);
 		
 };
 //------------------------------Acá se acaba la structura Point-------------------------------
@@ -29,32 +35,38 @@ class Qtree {
 	private:
 		struct QtreeNode {
 			Point Value;
+			bool leaf;
 			QtreeNode *NorthWest;
 			QtreeNode *NorthEast;
 			QtreeNode *SouthWest;
 			QtreeNode *SouthEast;
-			QtreeNode *Padre;
-			// Node(Point v, Node *NW = nullptr, Node *NE = nullptr, Node *SW = nullptr, Node *SE = nullptr): Value{v}, NorthWest{NW}, NorthEast{NE}, SouthWest{SW}, SouthEast{SE} {}
+			QtreeNode *Padre;	//devolver
+			
+			// QtreeNode(Point v, Node *NW = nullptr, Node *NE = nullptr, Node *SW = nullptr, Node *SE = nullptr): Value{v}, NorthWest{NW}, NorthEast{NE}, SouthWest{SW}, SouthEast{SE} {}
 			
 		};
 		
-		//-------------- Bounds // Esquinas -------------
-		Point ArribaIz = Point(0,0);
-		Point AbajoDer = Point(400,400);
 		
+		/*QtreeNode * Successor(QtreeNode *root)const;
+		QtreeNode * Predecessor(QtreeNode *root)const;*/
 		
-		QtreeNode * Successor(QtreeNode *root)const;
-		QtreeNode * Predecessor(QtreeNode *root)const;
+		void remove(QtreeNode * &root, Point p);
+		void insert(QtreeNode * &root, Point p);
+		void clear(QtreeNode * &root);
+		QtreeNode * find(QtreeNode *root, Point p)const;
+		
+		//size_t Num_Leaves(QtreeNode *root)const;
 		
 		Node *Quadtree = nullptr;
+		size_t count = 0;
 
 	public:
 		Qtree() = default;
 		~Qtree();
-		void insert(Node *nuevo);
-		bool inBounds(Point p);
-		QtreeNode * Search(Point p);
-		void remove(QtreeNode Point p);
+		void insert(Point p);
+		QtreeNode find(Point p)const;//not sure
+		void remove(Point p);// no estoy seguro de que quitar
+		void clear(void);
 		
 };
 //---------------------------Acá se acaba la clase qtree-------------------------------------------------
